98398a58b55999a5c1e41bb21b35fe4361fde57d
yves848 <yves.godart@gmail.com>
2022-06-10 14:33:21 +0200

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'advalvas-4ddf0',
+    appId: '1:604799176014:web:5183e4cea160666087a21f',
+    databaseURL: 'https://advalvas-4ddf0-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'advalvas-4ddf0.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCStfnn7PHRt3I_fynHGoKMtucbqcLFNT8',
+    authDomain: 'advalvas-4ddf0.firebaseapp.com',
+    messagingSenderId: '604799176014',
+  },
   production: true
 };
